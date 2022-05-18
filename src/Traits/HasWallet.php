a49cbc459237818e23a95abf22287233a0abc048
smitoi <stefan.mitoi27@gmail.com>
2022-05-18 22:44:14 +0300

    Small refactor to allow custom column name
    Include PHPDocs for HasWallet trait functions
    
    Refactor to use custom column directly instead of force filling

--- src/Traits/HasWallet.php
+++ src/Traits/HasWallet.php
@@ -8,46 +8,58 @@ use Stephenjude\Wallet\Exceptions\InvalidAmountException;
 
 trait HasWallet
 {
-    public function deposit(int|float $amount): float|int
+    protected string $walletColumnName = 'balance';
+
+    protected function getBalanceValue(): float|int
     {
-        $this->throwExceptionIfAmountIsInvalid($amount);
-
-        $balance = $this->wallet_balance ?? 0;
-
-        $balance += $amount;
-
-        $this->forceFill(['wallet_balance' => $balance])->save();
-
-        return $balance;
-    }
-
-    public function withdraw(int|float $amount): float|int
-    {
-        $this->throwExceptionIfAmountIsInvalid($amount);
-
-        $this->throwExceptionIfFundIsInsufficient($amount);
-
-        $balance = $this->wallet_balance - $amount;
-
-        $this->forceFill(['wallet_balance' => $balance])->save();
-
-        return $balance;
-    }
-
-    public function canWithdraw(int|float $amount): bool
-    {
-        $this->throwExceptionIfAmountIsInvalid($amount);
-
-        $balance = $this->wallet_balance ?? 0;
-
-        return $balance >= $amount;
+        return $this->{$this->walletColumnName} ?? 0;
     }
 
     public function balance(): Attribute
     {
-        return Attribute::get(fn() => $this->wallet_balance ?? 0);
+        return Attribute::get(fn() => $this->getBalanceValue());
     }
 
+    /**
+     * @param int|float $amount
+     * @return float|int
+     * @throws InvalidAmountException
+     */
+    public function deposit(int|float $amount): float|int
+    {
+        $this->throwExceptionIfAmountIsInvalid($amount);
+        $this->{$this->walletColumnName} += $amount;
+        $this->save();
+        return $this->getBalanceValue();
+    }
+
+    /**
+     * @param int|float $amount
+     * @return float|int
+     * @throws InsufficientFundException
+     * @throws InvalidAmountException
+     */
+    public function withdraw(int|float $amount): float|int
+    {
+        $this->throwExceptionIfAmountIsInvalid($amount);
+        $this->throwExceptionIfFundIsInsufficient($amount);
+        $this->{$this->walletColumnName} -= $amount;
+        $this->save();
+        return $this->getBalanceValue();
+    }
+
+    /**
+     * @throws InvalidAmountException
+     */
+    public function canWithdraw(int|float $amount): bool
+    {
+        $this->throwExceptionIfAmountIsInvalid($amount);
+        return $this->getBalanceValue() >= $amount;
+    }
+
+    /**
+     * @throws InvalidAmountException
+     */
     public function throwExceptionIfAmountIsInvalid(int|float $amount): void
     {
         if ($amount <= 0) {
@@ -55,6 +67,10 @@ trait HasWallet
         }
     }
 
+    /**
+     * @throws InsufficientFundException
+     * @throws InvalidAmountException
+     */
     public function throwExceptionIfFundIsInsufficient(int|float $amount): void
     {
         if (!$this->canWithdraw($amount)) {
